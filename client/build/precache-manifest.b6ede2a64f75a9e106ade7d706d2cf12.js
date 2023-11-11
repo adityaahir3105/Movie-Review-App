f933423c8946edf19298d15c1962cba221418776
adityaahir <ahiraditya31@gmail.com>
2023-11-11 18:30:58 +0530

    Added Build

---
+++ client/build/precache-manifest.b6ede2a64f75a9e106ade7d706d2cf12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535c6b4a5092ec6ab4898649dad6232f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a02d018eb61d8fa0d8",
+    "url": "/static/css/2.2d1346fc.chunk.css"
+  },
+  {
+    "revision": "ac39efc0ed61b1ac5668",
+    "url": "/static/css/main.6c28e13b.chunk.css"
+  },
+  {
+    "revision": "49a02d018eb61d8fa0d8",
+    "url": "/static/js/2.c35f1485.chunk.js"
+  },
+  {
+    "revision": "ac39efc0ed61b1ac5668",
+    "url": "/static/js/main.00337208.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
